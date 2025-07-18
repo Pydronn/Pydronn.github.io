938d6d33b0042429e3e52543ca1f21ba7e5e7f84
Pydronn <johannes@waldhaeusl.com>
2025-07-18 06:22:35 +0200

    Code changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
+// Store a reference to the <h1> in a variable
+const myHeading = document.querySelector("h1");
 const listItems = document.querySelectorAll("li");
+// Update the text content of the <h1>
+myHeading.textContent = "Hello world!";
 
 function toggleDone(e) {
   if (!e.target.className) {
