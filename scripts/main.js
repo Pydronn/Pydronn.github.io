dc468e1a31e68fd1898c6dd0c7b8193a20830038
Pydronn <johannes@waldhaeusl.com>
2025-07-18 06:42:03 +0200

    bug fix

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,8 @@ const myImage = document.querySelector("img");
 myImage.addEventListener("click", () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/Sequoia_Lab.png") {
-    myImage.setAttribute("src", "images/Sequoia_Lab.png");
-  } else {
     myImage.setAttribute("src", "images/WostiT.png");
+  } else {
+    myImage.setAttribute("src", "images/Sequoia_Lab.png");
   }
 });
